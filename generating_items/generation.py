2e02a5dd30085b22a442b7bc40e497efe5cd38d3
butyusya <divolyanskiy@edu.hse.ru>
2025-12-01 16:39:51 +0300

    added comments

--- generating_items/generation.py
+++ generating_items/generation.py
@@ -1,14 +1,24 @@
 from random import shuffle, choice, sample
 
+# возможные значения параметров
 forms = [
     ['circle', 'triangle', 'square', 'pentagon', 'hexagon'],
     ['red', 'yellow', 'green', 'blue', 'violet'],
     ['top-left', 'top-right', 'bottom-left', 'bottom-right', 'center']
 ]
 
+# все фигуры
 coords = [(x, y, z) for x in forms[0] for y in forms[1] for z in forms[2]]
 
 def group(a, posa, b, posb):
+    """
+    Функция для разбиения фигур на группы относительно двух зафиксированных параметров.
+    :param a: значение первого параметра
+    :param posa: тип параметра от 0 до 2
+    :param b: значение второго параметра
+    :param posb: тип параметра от 0 до 2
+    :returns: три списка. в первом оба параметра совпадают с целевыми, во втором ровно один, в третьем ни одного.
+    """
     both, one, none = [], [], []
     for c in coords:
         if c[posa] == a and c[posb] == b:
@@ -20,6 +30,16 @@ def group(a, posa, b, posb):
     return both, one, none
 
 def get_test(a, posa, b, posb, strong, target):
+    """
+    Функция для генерации одного теста.
+    :param a: значение первого параметра
+    :param posa: тип параметра от 0 до 2
+    :param b: значение второго параметра
+    :param posb: тип параметра от 0 до 2
+    :param strong: вторая операция из пары (and / xor)
+    :param target: тип фигуры в вопросе (yes / no / target)
+    :returns: в первом списке 4 кортежа со значениями параметров типа YES, во втором типа NO. кортеж с параметрами вопроса.
+    """
     both, one, none = group(a, posa, b, posb)
     shuffle(both)
     shuffle(one)
@@ -45,6 +65,13 @@ def get_test(a, posa, b, posb, strong, target):
     return yes, no, quest
 
 def get_practice(a, posa, target):
+    """
+    Функция для генерации тренировки.
+    :param a: значение параметра
+    :param posa: тип параметра от 0 до 2
+    :param target: тип фигуры в вопросе (yes / no)
+    :returns: в первом списке 4 кортежа со значениями параметров типа YES, во втором типа NO. кортеж с параметрами вопроса.
+    """
     true = [c for c in coords if c[posa] == a]
     false = [c for c in coords if c[posa] != a]
     shuffle(true)
@@ -56,6 +83,11 @@ def get_practice(a, posa, target):
     return true[:4], false[:4], quest
 
 def generate():
+    """
+    Функция для генерации всего.
+    :returns: список из 20 вопросов. в вопросе 2 кортежа: в первом описывается тест, во втором фигуры.
+        описание фигур -- в первом списке 4 кортежа со значениями параметров типа YES, во втором типа NO. кортеж с параметрами вопроса.
+    """
     practice = []
     posa1, posa2 = sample([0, 1, 2], 2)
     a1 = choice(forms[posa1])
